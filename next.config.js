681680f02f5080dfc05bcbc0c2a0be670e771f9a
mikefox17 <mikefox17@gmail.com>
2021-11-08 17:35:05 -0500

    dom

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     reactStrictMode: true,
     target: 'serverless',
     images: {
-        domains: ['https://hyperbros.wpengine.com/graphql'],
+        domains: ['localhost'],
     },
 };
